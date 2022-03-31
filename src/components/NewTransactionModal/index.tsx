d4ad472eace819044b33c094296d386047792942
Antonio Silva <antoniopsilva083@gmail.com>
2022-03-31 01:09:08 -0300

    feat: create modal styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -11,7 +12,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
     >
+      <button type="button" onClick={onRequestClose} className='react-modal-close'>
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+
       <Container>
         <h2>Register Transaction</h2>
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -83,4 +83,18 @@ button {
   position: relative;
   border-radius: 0.24rem;
 }
+
+.react-modal-close {
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+  border: 0;
+  background: transparent;
+
+  transition: filter 0.2s;                                                 wq
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+}
 `
