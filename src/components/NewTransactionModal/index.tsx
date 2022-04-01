2b28eb9c9d8b191c8de94c773f052c96913c1145
Antonio Silva <antoniopsilva083@gmail.com>
2022-04-01 01:22:21 -0300

    feat: create button types

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -31,6 +37,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           type="number"
           placeholder="Value"
         />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Input" />
+            <span>Input</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Output" />
+            <span>Output</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input
           placeholder="Category"
